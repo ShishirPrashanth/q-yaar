549f5480a7feefda17ed7f11ddf40fefd01252bd
Shishir Prashanth <shishir@esamudaay.com>
2024-07-30 23:36:33 +0530

    changed all dependencies thanks to the folder name change

--- q_yaar_platform/q_yaar/urls.py
+++ q_yaar_platform/q_yaar/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for q_yaar_platform project.
+URL configuration for q_yaar project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
