d046e2e1f5432849f2d774b410473450dbe0269c
Shishir Prashanth <shishir@esamudaay.com>
2024-08-08 17:00:52 +0530

    corrected wrong token expiry

--- q_yaar_platform/q_yaar/settings.py
+++ q_yaar_platform/q_yaar/settings.py
@@ -191,8 +191,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=365),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=365),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
