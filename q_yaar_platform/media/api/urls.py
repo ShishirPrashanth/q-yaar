665834209e2f6c7ac861836ad95d1f437712a583
Sireesh Kodali <sireesh@siruknown.net>
2026-09-08 23:29:56 +0530

    media: don't shadow more specific routes

--- q_yaar_platform/media/api/urls.py
+++ q_yaar_platform/media/api/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "media"
 
 urlpatterns = [
-    path(r"", views.AssetListView.as_view(), name="handler-asset-list"),
-    path(r"<uuid:asset_id>", views.AssetDetailView.as_view(), name="handler-asset-detail"),
     path(r"<uuid:asset_id>/confirm", views.AssetConfirmView.as_view(), name="handler-asset-confirm"),
+    path(r"<uuid:asset_id>", views.AssetDetailView.as_view(), name="handler-asset-detail"),
+    path(r"", views.AssetListView.as_view(), name="handler-asset-list"),
 ]
